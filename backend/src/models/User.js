251aaea58d9d2058702b40be191b1459bb88ea06
blytheb <builtbyblythe@gmail.com>
2025-09-05 12:34:43 -1000

    defined mongo schemas

---
+++ backend/src/models/User.js
@@ -0,0 +1,11 @@
+//User - store parent info and firbase uid
+//profile - profile info linked to user
+//card - card info linked to profile
+//trade - trade info linked to profile
+//tradeCard - card info linked to trade
+
+//User - uid, email, name, phone, profiles[]
+//profile - name, avatar, cards[], trades[]
+//card - name, set, number, condition, imageUrl
+//trade - type (offer/request), status (open/closed), cardsOffered[], cardsRequested[], createdAt
+//tradeCard - cardId, tradeId
