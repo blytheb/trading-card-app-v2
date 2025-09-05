6261191397fe93ef207ea53ffb4439b4e7d1b566
blytheb <builtbyblythe@gmail.com>
2025-09-05 00:19:07 -1000

    connected successfully to mongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,29 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import { connectDB } from "./config/db.js";
+
 dotenv.config();
 
 const app = express();
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
+//routes
 app.get("/", (req, res) => {
     res.send("Backend running !");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`)
+    });
+}).catch((error) => {
+    console.error("Failed to connect to the database", error);
 });
+
+
